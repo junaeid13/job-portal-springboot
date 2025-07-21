099a1958bba57454f95e3ca5db25f46e06ebad6d
junaeid ahmed <junaeidahmed13@gmail.com>
2025-07-21 13:47:48 +0100

    initial commit for job portal project

---
+++ src/test/java/com/junaeid/jobportal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junaeid.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
