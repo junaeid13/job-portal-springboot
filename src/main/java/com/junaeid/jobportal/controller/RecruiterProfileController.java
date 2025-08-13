47af63f1d03a93bb96b3308c7c1367741d79d2f9
junaeid ahmed <junaeidahmed13@gmail.com>
2025-08-13 13:05:09 +0100

    feat: add recruiter profile image upload support Closes #12

--- src/main/java/com/junaeid/jobportal/controller/RecruiterProfileController.java
+++ src/main/java/com/junaeid/jobportal/controller/RecruiterProfileController.java
@@ -12,9 +12,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -46,5 +51,31 @@ public class RecruiterProfileController {
             }
         }
         return "recruiter_Profile";
+    }
+
+    public String addNew(
+            RecruiterProfile recruiterProfile,
+            @RequestParam("image") MultipartFile multipartFile,
+            Model model
+    ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new
+                    UsernameNotFoundException("Username not found"));
+            recruiterProfile.setUserAccountId(users.getUserId());
+            recruiterProfile.setUser(users);
+        }
+        model.addAttribute("recruiterProfile", recruiterProfile);
+        String fileName = "";
+        if (!multipartFile.getOriginalFilename().equals("")) {
+            fileName = StringUtils
+                    .cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+            recruiterProfile.setFirstName(fileName);
+        }
+        RecruiterProfile savedUser = recruiterProfileService.addNew(recruiterProfile);
+
+        String uploadDir = "photos/recruiter/" + savedUser.getUserAccountId();
+        return null;
     }
 }
